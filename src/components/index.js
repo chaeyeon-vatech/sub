26e3868d43b824da68a0b58057f431c400907a50
chaeyeon-vatech <chaeyeon.park@vatech.com>
2021-01-28 17:36:16 +0900

    Update Storyboard Changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export {default as PaymentSetting, not as NotFound} from "./PaymentSetting";
-export {default as MainPage} from "./MainPage";
-export {default as ModifyDialog} from "./ModifyDialog";
+export {default as ModifyDialog} from "./Dialog";
 export {default as NavBar} from "./NavBar";
 export {default as Transfer} from "./Table/Transferlist"
 export {default as OrderBoard} from "./OrderPage/OrderBoard"

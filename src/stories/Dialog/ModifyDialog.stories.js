78d6c5d34f061cdd97f555a5bbf02626dee7a477
chaeyeon-vatech <chaeyeon.park@vatech.com>
2021-01-29 09:54:43 +0900

    Test

--- src/stories/Dialog/ModifyDialog.stories.js
+++ src/stories/Dialog/ModifyDialog.stories.js
@@ -1,39 +1,39 @@
-import {Box, Button, makeStyles} from "@material-ui/core";
-import {action} from "@storybook/addon-actions";
-import React, {useState} from "react";
-import {ModifyDialog} from "../../components";
+import { Box, Button, makeStyles } from "@material-ui/core";
+import { action } from "@storybook/addon-actions";
+import React, { useState } from "react";
+import { ModifyDialog } from "../../components";
 
 const useStyles = makeStyles(() => ({
-    root: {
-        display: "flex",
-        height: 400,
-        width: 500,
-        position: "relative",
-        justifyContent: "center",
-        alignItems: "center",
-    },
+  root: {
+    display: "flex",
+    height: 400,
+    width: 500,
+    position: "relative",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 }));
 
 export default {
-    component: ModifyDialog,
-    title: "Dialog",
-    excludeStories: /.*Data$/,
+  component: ModifyDialog,
+  title: "Dialog",
+  excludeStories: /.*Data$/,
 };
 
 export const Default = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const onClose = response => {
-        action("Response")(response ? "Yes" : "No");
-        setOpen(false)
-    }
+  const onClose = response => {
+    action("Response")(response ? "Yes" : "No");
+    setOpen(false)
+  }
 
-    return (
-        <Box border={1} className={classes.root}>
-            <Button variant="contained" onClick={() => setOpen(true)}>주문 재작성</Button>
-            <ModifyDialog message={{text: "현재 주문"}} open={open} onClose={(response) => onClose(response)}/>
-        </Box>
-    );
+  return (
+    <Box border={1} className={classes.root}>
+      <Button variant="contained" onClick={() => setOpen(true)}>주문 재작성</Button>
+      <ModifyDialog message={{ text: "승진" }} open={open} onClose={(response) => onClose(response)} />
+    </Box>
+  );
 };
